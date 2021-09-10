e65e2158e75406dcf21f5b559436b57672ec44f1
Skinhead12 <edithr_isc@hotmail.com>
2021-09-10 11:10:02 -0500

    errores de validacion

--- src/lib/components/Home.js
+++ src/lib/components/Home.js
@@ -4,36 +4,55 @@ import { auth } from '../secret.js';
 
 export const Home = () => {
   const Homediv = document.createElement('div');
-  const logoPDP = document.createElement('img');
-  const h1Welcome = document.createElement('h1');
+  const airplane = document.createElement('img');
+  const logo = document.createElement('img');
+  const labelWelcome = document.createElement('label');
+  const passport = document.createElement('img');
+  // const divWhite = document.createElement('div');
   const buttonRegister = document.createElement('button');
   const buttonLogin = document.createElement('button');
   const labelOr = document.createElement('label');
   const buttonGoogle = document.createElement('button');
-  h1Welcome.textContent = 'Bienvenid@';
+
+  airplane.setAttribute('src', 'https://firebasestorage.googleapis.com/v0/b/pata-de-perro-3a9dd.appspot.com/o/avion.png?alt=media&token=1719248f-44c5-4d22-b32b-664e31992608');
+  airplane.id = 'airplane';
+  logo.setAttribute('src', 'https://firebasestorage.googleapis.com/v0/b/pata-de-perro-3a9dd.appspot.com/o/logo-pata-de-perro.png?alt=media&token=338afd9b-ad90-453b-9007-05416b3dda22');
+  logo.id = 'logo';
+  labelWelcome.textContent = 'Travelers conectados';
+  labelWelcome.id = 'welcome';
+  passport.setAttribute('src', 'https://firebasestorage.googleapis.com/v0/b/pata-de-perro-3a9dd.appspot.com/o/passport.png?alt=media&token=bd477459-fe1f-4c31-9b2b-33cd4692d145');
+  passport.id = 'passport';
+  //  divWhite.id = 'divWhite';
   buttonRegister.textContent = 'Regístrate';
+  buttonRegister.id = 'btnRegister';
   buttonLogin.textContent = 'Iniciar Sesión';
+  buttonLogin.id = 'btnLogin';
   labelOr.textContent = 'o';
+  labelOr.id = 'labelO';
   buttonGoogle.textContent = 'Continuar con Google';
+  buttonGoogle.id = 'btnGoogle';
 
   buttonRegister.addEventListener('click', () => onNavigate('/register'));
   buttonLogin.addEventListener('click', () => onNavigate('/login'));
-
   buttonGoogle.addEventListener('click', (e) => {
     e.preventDefault();
     const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider)
-      .then(() => {
-        window.location.assign('/wall');
-        console.log('sign up with google');
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        console.log(result);
+        onNavigate('/wall');
       })
       .catch((error) => {
-        console.error(error);
+        console.log(error);
       });
   });
 
-  Homediv.appendChild(logoPDP);
-  Homediv.appendChild(h1Welcome);
+  Homediv.appendChild(airplane);
+  Homediv.appendChild(logo);
+  Homediv.appendChild(labelWelcome);
+  Homediv.appendChild(passport);
+  // Homediv.appendChild(divWhite);
   Homediv.appendChild(buttonRegister);
   Homediv.appendChild(buttonLogin);
   Homediv.appendChild(labelOr);
--- src/lib/components/Register.js
+++ src/lib/components/Register.js
@@ -1,64 +1,96 @@
 // eslint-disable-next-line import/no-cycle
 import { onNavigate } from '../../main.js';
 import { registerUser } from '../firebase.js';
+import firebase, { auth } from '../secret.js';
 
 export const Register = () => {
   const Homediv = document.createElement('div');
-  Homediv.id = 'divRegister';
-  const formRegister = document.createElement('form');
-  formRegister.id = 'register-form';
-  Homediv.textContent = 'Regístrate';
-  const inputUserName = document.createElement('input');
-  inputUserName.placeholder = 'Username';
-  const inputEmail = document.createElement('input');
-  inputEmail.placeholder = 'Email';
-  inputEmail.id = 'email';
-  inputEmail.setAttribute('type', 'email');
-  const inputPass = document.createElement('input');
-  inputPass.placeholder = 'Password';
-  inputPass.id = 'password';
-  inputPass.setAttribute('type', 'password');
-  const confirmPass = document.createElement('input');
-  confirmPass.placeholder = 'Confirm Password';
-  confirmPass.setAttribute('type', 'password');
-  confirmPass.id = 'confirm-pass';
   const buttonHome = document.createElement('button');
   buttonHome.textContent = 'Regresar';
-  const buttonAceptar = document.createElement('button');
-  buttonAceptar.textContent = 'Aceptar';
-  const buttonGoogle = document.createElement('button');
-  buttonGoogle.textContent = 'Continuar con Google';
+
+  const labelRegister = document.createElement('label');
+  labelRegister.textContent = 'Regístrate';
+  labelRegister.id = 'labelRegister';
+
+  const labelSubtitle = document.createElement('label');
+  labelSubtitle.textContent = 'Inicia la aventura';
+  labelSubtitle.id = 'labelSub';
+  /*   const formRegister = document.createElement('form');
+  formRegister.setAttribute('type', 'submit');
+  formRegister.id = 'formRegister'; */
+  const inputUsername = document.createElement('input');
+  inputUsername.setAttribute('type', 'text');
+  inputUsername.placeholder = 'Nombre de usuario';
+  inputUsername.id = 'inputUsername';
+
+  const eyeShow = document.createElement('span');
+  eyeShow.id = 'showPass';
+
+  const eyeHide = document.createElement('span');
+  eyeHide.id = 'hidePass';
+
+  const inputEmail = document.createElement('input');
+  inputEmail.setAttribute('type', 'email');
+  inputEmail.placeholder = 'Correo electrónico';
+  inputEmail.id = 'inputEmail';
+
+  const inputPassword = document.createElement('input');
+  inputPassword.setAttribute('type', 'password');
+  inputPassword.placeholder = 'Contraseña';
+  inputPassword.id = 'inputPassword';
+
+  const inputPasswordConfirm = document.createElement('input');
+  inputPasswordConfirm.setAttribute('type', 'password');
+  inputPasswordConfirm.placeholder = 'Confirmación';
+  inputPasswordConfirm.id = 'inputConfirm';
+
+  const buttonRegister = document.createElement('button');
+  buttonRegister.setAttribute('type', 'submit');
+  buttonRegister.id = 'buttonRegister';
+  buttonRegister.textContent = 'Regístrate';
+
+  const buttonGoogleRegister = document.createElement('button');
+  buttonGoogleRegister.textContent = 'Registrarse con Google';
+  buttonGoogleRegister.id = 'btnGoogleRegister';
 
   buttonHome.addEventListener('click', () => onNavigate('/'));
 
-  buttonGoogle.addEventListener('click', (e) => {
+  buttonGoogleRegister.addEventListener('click', (e) => {
     e.preventDefault();
+    // eslint-disable-next-line import/no-named-as-default-member
     const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider)
-      .then(() => {
-        window.location.assign('/wall');
-        console.log('sign up with google');
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        console.log(result);
+        onNavigate('/wall');
       })
       .catch((error) => {
-        console.error(error);
+        console.log(error);
       });
   });
 
-  buttonAceptar.addEventListener('click', (e) => {
-    const emailRegister = document.getElementById('email');
-    const passwordRegister = document.getElementById('password');
+  buttonRegister.addEventListener('click', (e) => {
+    const emailRegister = Homediv.querySelector('#inputEmail').value;
+    const passwordRegister = Homediv.querySelector('#inputPassword').value;
     e.preventDefault();
     registerUser(emailRegister, passwordRegister);
+    console.log(registerUser);
     onNavigate('/login');
   });
-
-  Homediv.appendChild(inputUserName);
-  Homediv.appendChild(inputEmail);
-  Homediv.appendChild(inputPass);
-  Homediv.appendChild(confirmPass);
-  Homediv.appendChild(buttonAceptar);
-  Homediv.appendChild(buttonGoogle);
+  /*
+  Homediv.appendChild(formRegister); */
   Homediv.appendChild(buttonHome);
+  Homediv.appendChild(labelRegister);
+  Homediv.appendChild(labelSubtitle);
+  Homediv.appendChild(inputUsername);
+  Homediv.appendChild(inputEmail);
+  Homediv.appendChild(eyeShow);
+  Homediv.appendChild(eyeHide);
+  Homediv.appendChild(inputPassword);
+  Homediv.appendChild(inputPasswordConfirm);
+  Homediv.appendChild(buttonRegister);
+  Homediv.appendChild(buttonGoogleRegister);
 
   return Homediv;
 };
