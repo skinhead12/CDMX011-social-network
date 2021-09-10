f61e53c182d4097281f47874322d7b02dfcacbf3
Skinhead12 <edithr_isc@hotmail.com>
2021-09-10 12:09:22 -0500

    ocultar y mostrar contraseña

--- src/lib/components/Register.js
+++ src/lib/components/Register.js
@@ -15,29 +15,41 @@ export const Register = () => {
   const labelSubtitle = document.createElement('label');
   labelSubtitle.textContent = 'Inicia la aventura';
   labelSubtitle.id = 'labelSub';
-  /*   const formRegister = document.createElement('form');
-  formRegister.setAttribute('type', 'submit');
-  formRegister.id = 'formRegister'; */
+
   const inputUsername = document.createElement('input');
   inputUsername.setAttribute('type', 'text');
   inputUsername.placeholder = 'Nombre de usuario';
   inputUsername.id = 'inputUsername';
-
-  const eyeShow = document.createElement('span');
-  eyeShow.id = 'showPass';
-
-  const eyeHide = document.createElement('span');
-  eyeHide.id = 'hidePass';
 
   const inputEmail = document.createElement('input');
   inputEmail.setAttribute('type', 'email');
   inputEmail.placeholder = 'Correo electrónico';
   inputEmail.id = 'inputEmail';
 
+  const labelEmail = document.createElement('label');
+  labelEmail.textContent = 'El correo no es un formato valido';
+  labelEmail.id = 'labelEmail';
+  labelEmail.style.display = 'none';
+
   const inputPassword = document.createElement('input');
   inputPassword.setAttribute('type', 'password');
   inputPassword.placeholder = 'Contraseña';
   inputPassword.id = 'inputPassword';
+
+  const labelPassword = document.createElement('label');
+  labelPassword.textContent = 'Las contraseñas no coinciden';
+  labelPassword.id = 'labelPass';
+  labelPassword.style.display = 'none';
+
+  const eyeOff = document.createElement('img');
+  eyeOff.setAttribute('src', 'https://firebasestorage.googleapis.com/v0/b/pata-de-perro-3a9dd.appspot.com/o/outline_visibility_off_black_24dp.png?alt=media&token=981cfa55-bea9-47cb-a710-c27509e22066');
+  eyeOff.id = 'eyeOff';
+  eyeOff.style.display = 'block';
+
+  const eyeOn = document.createElement('img');
+  eyeOn.setAttribute('src', 'https://firebasestorage.googleapis.com/v0/b/pata-de-perro-3a9dd.appspot.com/o/outline_visibility_black_24dp.png?alt=media&token=655a1895-dbf3-4495-a2f5-7b1f1bf977b1');
+  eyeOn.id = 'eyeOn';
+  eyeOn.style.display = 'none';
 
   const inputPasswordConfirm = document.createElement('input');
   inputPasswordConfirm.setAttribute('type', 'password');
@@ -54,6 +66,24 @@ export const Register = () => {
   buttonGoogleRegister.id = 'btnGoogleRegister';
 
   buttonHome.addEventListener('click', () => onNavigate('/'));
+
+  eyeOff.addEventListener('click', () => {
+    const passwordValue = Homediv.querySelector('#inputPassword');
+    if (passwordValue.type === 'password') {
+      passwordValue.type = 'text';
+      eyeOff.style.display = 'none';
+      eyeOn.style.display = 'block';
+    }
+  });
+
+  eyeOn.addEventListener('click', () => {
+    const passwordValue = Homediv.querySelector('#inputPassword');
+    if (passwordValue.type === 'text') {
+      passwordValue.type = 'password';
+      eyeOff.style.display = 'block';
+      eyeOn.style.display = 'none';
+    }
+  });
 
   buttonGoogleRegister.addEventListener('click', (e) => {
     e.preventDefault();
@@ -73,21 +103,29 @@ export const Register = () => {
   buttonRegister.addEventListener('click', (e) => {
     const emailRegister = Homediv.querySelector('#inputEmail').value;
     const passwordRegister = Homediv.querySelector('#inputPassword').value;
+    const passwordConfirm = Homediv.querySelector('#inputConfirm').value;
     e.preventDefault();
-    registerUser(emailRegister, passwordRegister);
     console.log(registerUser);
-    onNavigate('/login');
+    if (passwordRegister !== passwordConfirm) {
+      labelPassword.style.display = 'block';
+    } else if (inputEmail.type !== 'email') {
+      labelEmail.style.display = 'block';
+    } else {
+      registerUser(emailRegister, passwordRegister);
+      onNavigate('/login');
+    }
   });
-  /*
-  Homediv.appendChild(formRegister); */
+
   Homediv.appendChild(buttonHome);
   Homediv.appendChild(labelRegister);
   Homediv.appendChild(labelSubtitle);
   Homediv.appendChild(inputUsername);
   Homediv.appendChild(inputEmail);
-  Homediv.appendChild(eyeShow);
-  Homediv.appendChild(eyeHide);
+  Homediv.appendChild(labelEmail);
   Homediv.appendChild(inputPassword);
+  Homediv.appendChild(labelPassword);
+  Homediv.appendChild(eyeOn);
+  Homediv.appendChild(eyeOff);
   Homediv.appendChild(inputPasswordConfirm);
   Homediv.appendChild(buttonRegister);
   Homediv.appendChild(buttonGoogleRegister);
